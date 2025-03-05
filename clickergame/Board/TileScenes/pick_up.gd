6f2ebb971c004ead370624a52c1f9d304cb596ed
Pete Santimauro <psantimauro@gmail.com>
2025-03-04 22:41:30 -0700

    updated sounds and some assets

--- clickergame/Board/TileScenes/pick_up.gd
+++ clickergame/Board/TileScenes/pick_up.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
 @export var type:TileManager.tiles = TileManager.tiles.PICKUP
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 func pickup():
+	audio_stream_player_2d.play()
 	Globals.item_picked_up.emit(type)
+	self.visible = false
+	await get_tree().create_timer(0.5).timeout
 	self.queue_free()
--- clickergame/MenuItems/tool_item.gd
+++ clickergame/MenuItems/tool_item.gd
@@ -2,7 +2,16 @@ class_name ToolItem
 extends PanelContainer
 @onready var _texture_rect: TextureRect = $TextureRect
 
+@export var tool_type:InventoryManager.tool_types
+
 @export var texture:Texture :
 	set(txtr):
 		_texture_rect.texture = txtr
+@onready var label: Label = $PanelContainer/Label
 		
+func _on_tool_level(type, value):
+	if type == tool_type:
+		label.text = str("%1.3f" % value)
+
+func _ready() -> void:
+	InventoryManager.tool_strength_changed.connect(_on_tool_level)
